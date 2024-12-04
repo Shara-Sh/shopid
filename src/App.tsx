f4ff10589daaa3382148e25f23f6ffa3a1733d5d
Shara <sharashahverdian@gmail.com>
2024-12-04 18:39:04 +0330

    - initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="flex h-screen flex-col items-center justify-center bg-slate-700 text-white">
+      <h1 className="text-6xl font-bold">ShopID</h1>
+      <h2 className="text-4xl">Comming Soon...</h2>
+    </div>
+  );
+}
+
+export default App;
